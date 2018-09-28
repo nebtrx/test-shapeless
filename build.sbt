83f876cef26b02087ff8ba496022e3886abf822e
Omar Ahmed <neb.trx@gmail.com>
2018-09-27 19:13:10 -0500

    Next change

--- build.sbt
+++ build.sbt
@@ -6,7 +6,17 @@ lazy val root = (project in file(".")).
     )),
     name := "empty shapeless template",
     description := "",
-    libraryDependencies += "com.chuusai" %% "shapeless" % shapelessVersion
+    libraryDependencies += "com.chuusai" %% "shapeless" % shapelessVersion,
+
+    scalacOptions ++= Seq(
+      "-Xlint",
+      "-Yno-adapted-args",
+      "-Ywarn-dead-code",        // N.B. doesn't work well with the ??? hole
+      "-Ywarn-numeric-widen",
+      "-Ywarn-value-discard",
+      "-Yliteral-types",
+      "-Ywarn-unused-import"     // 2.11 only
+    )
   )
 
 
--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -1,5 +1,10 @@
 import shapeless._
 import shapeless.Generic
+import shapeless.labelled.{FieldType, KeyTag}
+import shapeless.syntax.SingletonOps
+import shapeless.syntax.singleton._
+
+import shapeless.labelled.field
 
 object Example extends App {
 
@@ -16,4 +21,18 @@ object Example extends App {
   println(gen.to(Rectangle(3D, 3D)))
 
   println(gen.to(Circle(1.0)))
+
+  val aa: 42 = 42
+
+  trait Cherries
+
+  val numCherries2: Int with Cherries = 23.asInstanceOf[Int with Cherries]
+
+
+  val someNumber = 123
+
+  val numCherries: FieldType[SingletonOps#T, Int] = "numCherries" ->> someNumber
+
+  val f: FieldType[Cherries, Int] = field[Cherries](234)
+
 }
